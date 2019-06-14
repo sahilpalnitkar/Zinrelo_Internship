39c2a5748170f98361b4db423f9cdf343e512adb
Unknown <sahilspalnitkar@gmail.com>
2019-06-14 11:54:55 +0530

    FINAL
    Final with all comments and gitignore

--- url_request_driver.py
+++ url_request_driver.py
@@ -37,20 +37,20 @@ url3 = 'https://akshaysandbox4.mybigcommerce.com/garden/'
 url4 = 'https://akshaysandbox4.mybigcommerce.com/kitchen/'
 url5 = 'https://akshaysandbox4.mybigcommerce.com/publications/'
 
-choice = input("Enter the page number for which you want the min, max and average response times - ")
+choice = input("Enter the page number 1-5 for which you want the min, max and average response times - ")
 
 if choice == 1:
-	
 	print(url_hit_process(url1)) #Prints dict returned by function
-if choice == 2:
+elif choice == 2:
 	print(url_hit_process(url2)) #Prints dict returned by function
-if choice == 3:
+elif choice == 3:
 	print(url_hit_process(url3)) #Prints dict returned by function
-if choice == 4:
+elif choice == 4:
 	print(url_hit_process(url4)) #Prints dict returned by function
-if choice == 5:
+elif choice == 5:
 	print(url_hit_process(url5)) #Prints dict returned by function
-
+else:
+	print("Invalid URL choice")
 
 #ENDS MAIN AND PRINTS TIME TAKE TO EXECUTE ENTIRE PROGRAM
 print("--- Time taken to execute program was %s seconds ---" % (time.time() - start_time))
