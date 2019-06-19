1339bace61d796311ba9d865765ccf854deb85f7
Sahil Palnitkar <sahilspalnitkar@gmail.com>
2019-06-19 12:19:28 +0530

    Tested custom amazon cloudfront webpage and tested direct s3 bucket call

--- localhost_s3_bucket_benchmark.py
+++ localhost_s3_bucket_benchmark.py
@@ -6,7 +6,7 @@ import threading
 start_time = time.time()
 
 
-def url_hit_process_for_conf(url,times_hit):  # Method executes hits to the stored URLS and stores time data values
+def url_hit_process_for_conf(url, times_hit):  # Method executes hits to the stored URLS and stores time data values
     # times_hit = 100  # Creates random integer to decide how many times to hit URL
     min_val = 100
     max_val = 0
@@ -30,25 +30,26 @@ def url_hit_process_for_conf(url,times_hit):  # Method executes hits to the stor
                 'maximum value': max_val,
                 'average value': avg_val,
                 'times hit': times_hit}
-    f = open("benchmark.txt", "a")
+    f = open("{}_benchmark.txt".format("s3_cloudfront"), "a")
     f.write(json.dumps(url_dict))
     f.write("\n")
     f.close()
     return url_dict  # Returns dictionary to main function
 
 
-test_url = "http://yoda.shopsocially.com/js/zrl_conf?merchant_id=0564638b30"
+test_url = "http://d3sdpo4zrpd3b.cloudfront.net/b857a8dd91.js"
+# test_url = "https://zrlconf1.s3.amazonaws.com/b857a8dd91.js"
 # url_hit_process_for_conf(test_url)
 print("--- Time taken to execute program was %s seconds ---" % (time.time() - start_time))
 
-# t1 = threading.Thread(target=url_hit_process_for_conf, args=[test_url, 100])
-# t2 = threading.Thread(target=url_hit_process_for_conf, args=[test_url, 150])
-# t3 = threading.Thread(target=url_hit_process_for_conf, args=[test_url, 200])
-# t4 = threading.Thread(target=url_hit_process_for_conf, args=[test_url, 250])
+t1 = threading.Thread(target=url_hit_process_for_conf, args=[test_url, 100])
+t2 = threading.Thread(target=url_hit_process_for_conf, args=[test_url, 150])
+t3 = threading.Thread(target=url_hit_process_for_conf, args=[test_url, 200])
+t4 = threading.Thread(target=url_hit_process_for_conf, args=[test_url, 250])
 # t5 = threading.Thread(target=url_hit_process_for_conf, args=[test_url, 300])
-# t1.start()
-# t2.start()
-# t3.start()
-# t4.start()
+t1.start()
+t2.start()
+t3.start()
+t4.start()
 # t5.start()
-# print("--- Time taken to execute program was %s seconds ---" % (time.time() - start_time))
+print("--- Time taken to execute program was %s seconds ---" % (time.time() - start_time))
